f78b842bc3bea0ef70d2948e58a1f3f0ccedd15e
varunkumarnr <varunnrk@gmail.com>
2020-08-02 12:47:56 +0530

    origin master

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from 'react';
 import './Sidebar.css';
+import HomeIcon from '@material-ui/icons/Home';
+import WhatShotIcon from '@material-ui/icons/Whatshot';
+import SubscriptionIcon from '@material-ui/icons/Subscriptions';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import HistoryIcon from '@material-ui/icons/History';
+import OndemandVideoIcon from '@material-ui/icons/OndemandVideo';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import SidebarRow from './SidebarRow.js';
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>I am side bar </h1>
-            <Sidebarrow />
+            <SidebarRow selected Icon={HomeIcon} title="Home"/>
+            <SidebarRow Icon={WhatShotIcon} title="Trending"/>
+            <SidebarRow Icon={SubscriptionIcon} title="Subscriptions"/>
+            <hr />
+            <SidebarRow Icon={VideoLibraryIcon} title="Library"/>
+            <SidebarRow Icon={HistoryIcon} title="History" />
+            <SidebarRow Icon={OndemandVideoIcon} title="Your Videos" />
+            <SidebarRow Icon={WatchLaterIcon} title="Watch Later" />
+            <SidebarRow Icon={ThumbUpIcon} title="Liked Videos" />
+            <SidebarRow Icon={ExpandMoreIcon} title="Show More" />
         </div>
     )
 }
---
+++ src/SidebarRow.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+import './SidebarRow.css'
+function SidebarRow({selected,Icon,title}) {
+    return (
+        <div className = {`sidebarRow ${selected&&"selected"}`}>
+            <div className = "sidebarRow">
+            <Icon className='sidebarRow-Icon '/>
+            <h2 className="sidebarRow-title">{title}</h2>
+            
+           
+           </div>
+            
+        </div>
+    )
+}
+
+export default SidebarRow
